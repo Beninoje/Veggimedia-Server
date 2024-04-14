55b926cbfbc623b5206a62ce9fde4f2e174eb474
Beninoje <beninoje@hotmail.com>
2024-04-14 19:16:17 -0400

    ALL CRUD

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-//create express app
+// create express app
 const app = express();
 app.use(bodyParser.json());
 
-//dotenv production body
-if(process.env.NODE_ENV !== 'production')
-{
+// use .env if not in production mode
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 
     // cors for angular client access
@@ -19,22 +18,18 @@ if(process.env.NODE_ENV !== 'production')
     }));
 }
 
-
-
-//DB Connection
-mongoose.connect(process.env.MONGO_DB,{})
-.then((res)=>{console.log('Connected to MongoDB')})
-.catch((err)=>{console.log(`Connection error: ${err}`)})
+// db connect
+mongoose.connect(process.env.MONGO_DB, {})
+.then((res) => { console.log('Connected to MongoDB') })
+.catch((err) => { console.log(`Connection Failure: ${err}`)});
 
 // controller + route
 const mediaController = require('./controllers/media');
 app.use('/v1/api/media', mediaController);
 
-//route base URL to angular runtime inside public
+// route base URL to angular runtime inside public
 app.use(express.static(__dirname + '/public'));
-app.get("*",(req,res)=>res.sendFile(__dirname + '/public/index.html'));
+app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-app.listen(1000)
+app.listen(1000);
 module.exports = app;
-
-
